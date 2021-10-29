f368e9c18e1d5bf9e089ca7a35928080f6195c5f
pinheirogh <gabriel.pinheiroconceicao@gmail.com>
2021-10-28 22:15:04 -0300

    Atualização de paramatros e redirecionamento de funções

--- src/main/scala/TestPackage/Demo.scala
+++ src/main/scala/TestPackage/Demo.scala
@@ -4,51 +4,51 @@ import scala.collection.immutable.ListMap
 
 object Demo {
   def main(args: Array[String]): Unit ={
-    var lines = scala.io.Source.fromFile("teste.txt").mkString
-    print(lines.replaceAll("/^\\w+$/", ""))
-    sort(ListMap("Linha" -> 4, "teste" -> 1, "alguma" -> 3, "coisa" -> 2), no_op(null))
-    // normalize(lines, no_op(null))
+    var path = "teste.txt"
+    //print(lines.replaceAll("/^\\w+$/", ""))
+    // sort(ListMap("Linha" -> 4, "teste" -> 1, "alguma" -> 3, "coisa" -> 2), no_op(null))
+    read_file(path, no_op(null))
   }
 
   def no_op(tt: Null): Unit = {
     val donothing = tt
   }
 
-  def read_file(path_to_file: String, fn: (String) => String): (Unit)={
+  def read_file(path_to_file: String, fn: Unit): (Unit)={
     val data = Source.fromFile(path_to_file).getLines.mkString
-    normalize(data, fn)
+    filter_chars(data, fn)
   }
 
-  def filter_chars(str_data: String, fn: (String) => String): (Unit)={
+  def filter_chars(str_data: String, fn: Unit): (Unit)={
     val pattern = str_data.replaceAll("/^\\w+$/", "")
-    scan_function(pattern, fn)
+    normalize(pattern, fn)
   }
 
-  def normalize(str_data: String, fn: (String) => String): (Unit)={
+  def normalize(str_data: String, fn: Unit): (Unit)={
     var lines = str_data.toLowerCase()
-    scan_function(lines, no_op(null))
+    scan_function(lines, fn)
   }
 
-  def scan_function(str_data: String, unit: Unit): Unit = {
-    var wordlist = str_data.split('\n')
+  def scan_function(str_data: String, fn: Unit): Unit = {
+    var wordlist = str_data.split(',')
     var wordBufferList = ListBuffer[String]()
     for (word <- wordlist) wordBufferList += word
-    remove_stop_words(wordBufferList, no_op(null))
+    remove_stop_words(wordBufferList, fn)
   }
 
-  def remove_stop_words(tt: ListBuffer[String], unit: Unit): Unit ={
+  def remove_stop_words(tt: ListBuffer[String], fn: Unit): Unit ={
     for (word <- tt) println(word)
-    // frequencies(tt, no_op(null))
+    // frequencies(tt, fn)
   }
 
-  def frequencies(tt: ListBuffer[String], unit: Unit): Unit = {
+  def frequencies(tt: ListBuffer[String], fn: Unit): Unit = {
     var wf = new ListMap[String, Int]()
-    sort(wf, no_op(null))
+    sort(wf, fn)
   }
 
-  def sort(wf: ListMap[String, Int], unit: Unit): Unit = {
+  def sort(wf: ListMap[String, Int], fn: Unit): Unit = {
     val a = 0
-    print_text(ListMap(wf.toSeq.sortWith(_._2 > _._2):_*), no_op(null))
+    print_text(ListMap(wf.toSeq.sortWith(_._2 > _._2):_*), fn)
   }
 
   def print_text(wf: ListMap[String, Int], unit: Unit): Unit = {
