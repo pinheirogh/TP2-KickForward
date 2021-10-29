a3a3d7212defcb24b03b3d464bbb1d95ce7c9a97
luizangomes <luizangomes@outlook.com>
2021-10-28 21:50:02 -0300

    commit to luiza's branch

--- src/main/scala/TestPackage/Demo.scala
+++ src/main/scala/TestPackage/Demo.scala
@@ -1,26 +1,30 @@
-import scala.io.Source, scala.collection.mutable.ListBuffer
+import scala.io.Source
+import scala.collection.mutable.ListBuffer
+import scala.collection.immutable.ListMap
 
 object Demo {
   def main(args: Array[String]): Unit ={
     var lines = scala.io.Source.fromFile("teste.txt").mkString
-    normalize(lines, no_op(null))
+    print(lines.replaceAll("/^\\w+$/", ""))
+    sort(ListMap("Linha" -> 4, "teste" -> 1, "alguma" -> 3, "coisa" -> 2), no_op(null))
+    // normalize(lines, no_op(null))
   }
 
   def no_op(tt: Null): Unit = {
     val donothing = tt
   }
 
-  def read_file(path_to_file: String, fn: (String) => String): (String)={
+  def read_file(path_to_file: String, fn: (String) => String): (Unit)={
     val data = Source.fromFile(path_to_file).getLines.mkString
-    normalize(data)
-  }
-  
-  def filter_chars(str_data: String, fn: (String) => String): (String)={
-      val pattern = str_data.replaceAll("\\W", "")
-      scan(pattern)
+    normalize(data, fn)
   }
 
-  def normalize(str_data: String, unit: Unit): Unit ={
+  def filter_chars(str_data: String, fn: (String) => String): (Unit)={
+    val pattern = str_data.replaceAll("/^\\w+$/", "")
+    scan_function(pattern, fn)
+  }
+
+  def normalize(str_data: String, fn: (String) => String): (Unit)={
     var lines = str_data.toLowerCase()
     scan_function(lines, no_op(null))
   }
@@ -34,6 +38,23 @@ object Demo {
 
   def remove_stop_words(tt: ListBuffer[String], unit: Unit): Unit ={
     for (word <- tt) println(word)
+    // frequencies(tt, no_op(null))
+  }
+
+  def frequencies(tt: ListBuffer[String], unit: Unit): Unit = {
+    var wf = new ListMap[String, Int]()
+    sort(wf, no_op(null))
+  }
+
+  def sort(wf: ListMap[String, Int], unit: Unit): Unit = {
+    val a = 0
+    print_text(ListMap(wf.toSeq.sortWith(_._2 > _._2):_*), no_op(null))
+  }
+
+  def print_text(wf: ListMap[String, Int], unit: Unit): Unit = {
+    wf.foreach {
+      case (key, value) => println (key + " - " + value)
+    }
   }
 
 }
