287734e6db4aa012421f4ddcc77ceaa621378417
Pedenite <pedenite@gmail.com>
2021-11-02 11:52:59 -0300

    Deixando alfanumericos

--- src/main/scala/TestPackage/Demo.scala
+++ src/main/scala/TestPackage/Demo.scala
@@ -21,7 +21,7 @@ object Demo {
   }
 
   def filter_chars(str_data: String, fn: Unit): (Unit)={
-    val pattern = str_data.replaceAll("[^A-Za-z ]", "")
+    val pattern = str_data.replaceAll("[^A-Za-z0-9 ]", "")
     normalize(pattern, fn)
   }
 
