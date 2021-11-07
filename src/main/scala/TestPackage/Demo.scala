d4f5f5fc369eef1caa0c730851c136f0d2dfc2e1
Luiza N Gomes <37775707+luizangomes@users.noreply.github.com>
2021-11-07 10:05:48 -0300

    Update Demo.scala

--- src/main/scala/TestPackage/Demo.scala
+++ src/main/scala/TestPackage/Demo.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable.HashMap
 
 object Demo {
   def main(args: Array[String]): Unit ={
-    var path = "teste.txt"
+    var path = "teste1.txt" //The tests are "texte1.txt", "teste2.txt and "teste3.txt"
 
     read_file(path, no_op(null))
   }
